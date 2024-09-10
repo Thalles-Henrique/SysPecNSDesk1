37a86ec26800b5f36938ec729a186bbe94182439
thalles.htborges <thalles.htborges@ITQ0675985W10-1>
2024-09-10 08:08:30 -0300

    Classe Fornecedor e Produto concluida

--- SysPecNSLib1/Fornecedor.cs
+++ SysPecNSLib1/Fornecedor.cs
@@ -1,12 +1,132 @@
-﻿using System;
+﻿using MySqlX.XDevAPI;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SysPecNSLib1
+namespace SysPecNSLib
 {
-    internal class Fornecedor
+    public class Fornecedor
     {
+        public int Id { get; set; }
+        public string? RazaoSocial { get; set; }
+        public string? Fantasia { get; set; }
+        public string? Cnpj { get; set; }
+        public string? Contato { get; set; }
+        public string? Telefone { get; set; }
+        public string? Email { get; set; }
+
+        public Fornecedor()
+        {
+
+        }
+        public Fornecedor(int id, string? razaoSocial, string? fantasia, string? cnpj, string? contato, string? telefone, string? email)
+        {
+            Id = id;
+            RazaoSocial = razaoSocial;
+            Fantasia = fantasia;
+            Cnpj = cnpj;
+            Contato = contato;
+            Telefone = telefone;
+            Email = email;
+        }
+        public Fornecedor(string? razaoSocial, string? fantasia, string? cnpj, string? contato, string? telefone, string? email)
+        {
+            RazaoSocial = razaoSocial;
+            Fantasia = fantasia;
+            Cnpj = cnpj;
+            Contato = contato;
+            Telefone = telefone;
+            Email = email;
+        }
+
+        public void Inserir()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "sp_fornecedor_insert";
+            cmd.Parameters.AddWithValue("sprazao_social", RazaoSocial);
+            cmd.Parameters.AddWithValue("spfantasia", Fantasia);
+            cmd.Parameters.AddWithValue("spcnpj", Cnpj);
+            cmd.Parameters.AddWithValue("spcontato", Contato);
+            cmd.Parameters.AddWithValue("sptelefone", Telefone);
+            cmd.Parameters.AddWithValue("spemail", Email);
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Id = dr.GetInt32(0);
+            }
+        }
+        public static Fornecedor ObterPorId(int id)
+        {
+            Fornecedor fornecedor = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = $"select * from fornecedores where id = {id}";
+            var dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                fornecedor = new(
+                   dr.GetInt32(0),
+                   dr.GetString(1),
+                   dr.GetString(2),
+                   dr.GetString(3),
+                   dr.GetString(4),
+                   dr.GetString(5),
+                   dr.GetString(6)
+                   );
+            }
+            return fornecedor;
+
+        }
+        public static List<Fornecedor> ObterLista(string? fantasia = "")
+        {
+            List<Fornecedor> lista = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            if (fantasia == "")
+            {
+                cmd.CommandText = "select * from fornecedores order by fantasia";
+            }
+            else
+            {
+                cmd.CommandText = $"select * from fornecedores where nome Like '%{fantasia}%' order by fantasia";
+            }
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                lista.Add(
+                    new(
+                        dr.GetInt32(0),
+                        dr.GetString(1),
+                        dr.GetString(2),
+                        dr.GetString(3),
+                        dr.GetString(4),
+                        dr.GetString(5),
+                        dr.GetString(6)
+                        )
+                    );
+            }
+            return lista;
+        }
+        public void Atualizar()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_fornecedor_update";
+            cmd.Parameters.AddWithValue("spid", Id);
+            cmd.Parameters.AddWithValue("sprazao_social", RazaoSocial);
+            cmd.Parameters.AddWithValue("spfantasia", Fantasia);
+            cmd.Parameters.AddWithValue("spcnpj", Cnpj);
+            cmd.Parameters.AddWithValue("spcontato", Contato);
+            cmd.Parameters.AddWithValue("sptelefone", Telefone);
+            cmd.Parameters.AddWithValue("spemail", Email);
+            cmd.ExecuteNonQuery();
+            ;
+        }
     }
 }
--- SysPecNSLib1/Produto.cs
+++ SysPecNSLib1/Produto.cs
@@ -3,10 +3,149 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
-namespace SysPecNSLib1
+namespace SysPecNSLib
 {
-    internal class Produto
+    public class Produto
     {
+        public int Id { get; set; }
+        public string? CodBar { get; set; }
+        public string? Descricao { get; set; }
+        public double? ValorUnit { get; set; }
+        public string? UnidadeVenda { get; set; }
+        public Categoria? Categoria { get; set; }
+        public double EstoqueMinimo { get; set; }
+        public double ClasseDesconto { get; set; }
+        public byte[]? Imagem { get; set; }
+        public DateTime? DatCad { get; set; }
+
+        public Produto()
+        {
+        }
+        //metodo construtor usado com procedure para inserir
+        public Produto(string? codBar, string? descricao, double? valorUnit, string? unidadeVenda, Categoria? categoria, double estoqueMinimo, double classeDesconto)
+        {
+            CodBar = codBar;
+            Descricao = descricao;
+            ValorUnit = valorUnit;
+            UnidadeVenda = unidadeVenda;
+            Categoria = categoria;
+            EstoqueMinimo = estoqueMinimo;
+            ClasseDesconto = classeDesconto;
+        }
+        public Produto(string? codBar, string? descricao, double? valorUnit, string? unidadeVenda, Categoria? categoria, double estoqueMinimo, double classeDesconto, byte[]? imagem)
+        {
+            CodBar = codBar;
+            Descricao = descricao;
+            ValorUnit = valorUnit;
+            UnidadeVenda = unidadeVenda;
+            Categoria = categoria;
+            EstoqueMinimo = estoqueMinimo;
+            ClasseDesconto = classeDesconto;
+            Imagem = imagem;
+        }
+        public Produto(string? codBar, string? descricao, double? valorUnit, string? unidadeVenda, Categoria? categoria, double estoqueMinimo, double classeDesconto, byte[]? imagem, DateTime? datCad)
+        {
+            CodBar = codBar;
+            Descricao = descricao;
+            ValorUnit = valorUnit;
+            UnidadeVenda = unidadeVenda;
+            Categoria = categoria;
+            EstoqueMinimo = estoqueMinimo;
+            ClasseDesconto = classeDesconto;
+            Imagem = imagem;
+            DatCad = datCad;
+        }
+        public Produto(int id, string? codBar, string? descricao, double? valorUnit, string? unidadeVenda, Categoria? categoria, double estoqueMinimo, double classeDesconto, byte[]? imagem, DateTime? datCad)
+        {
+            Id = id;
+            CodBar = codBar;
+            Descricao = descricao;
+            ValorUnit = valorUnit;
+            UnidadeVenda = unidadeVenda;
+            Categoria = categoria;
+            EstoqueMinimo = estoqueMinimo;
+            ClasseDesconto = classeDesconto;
+            Imagem = imagem;
+            DatCad = datCad;
+        }
+        //Inserindo produto com procedure
+        public void Inserir()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_produto_insert";
+            cmd.Parameters.AddWithValue("spcod_barras", CodBar);
+            cmd.Parameters.AddWithValue("spdescricao", Descricao);
+            cmd.Parameters.AddWithValue("spvalor_unit", ValorUnit);
+            cmd.Parameters.AddWithValue("spunidade_venda", UnidadeVenda);
+            cmd.Parameters.AddWithValue("spcategoria_id", Categoria.Id);
+            cmd.Parameters.AddWithValue("spestoque_minimo", EstoqueMinimo);
+            cmd.Parameters.AddWithValue("spclasse_desconto", ClasseDesconto);
+            Id = Convert.ToInt32(cmd.ExecuteScalar());
+        }
+        //Atualizando os produtos com procedure
+        public void Atualizar()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_produto_update";
+            cmd.Parameters.AddWithValue("spid", Id);
+            cmd.Parameters.AddWithValue("spcod_barras", CodBar);
+            cmd.Parameters.AddWithValue("spdescricao", Descricao);
+            cmd.Parameters.AddWithValue("spvalor_unit", ValorUnit);
+            cmd.Parameters.AddWithValue("spunidade_venda", UnidadeVenda);
+            cmd.Parameters.AddWithValue("spcategoria_id", Categoria.Id);
+            cmd.Parameters.AddWithValue("spestoque_minimo", EstoqueMinimo);
+            cmd.Parameters.AddWithValue("spclasse_desconto", ClasseDesconto);
+            cmd.ExecuteNonQuery();
+        }
+        public static Produto ObterPorId(int id)
+        {
+            Produto produto = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandText = $"select * from produtos where id = {id}";
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                produto = new(
+                    dr.GetInt32(0),
+                    dr.GetString(1),
+                    dr.GetString(2),
+                    dr.GetDouble(3),
+                    dr.GetString(4),
+                    Categoria.ObterPorId(dr.GetInt32(5)),
+                    dr.GetDouble(6),
+                    dr.GetDouble(7),
+                    null,
+                    dr.GetDateTime(9)
+                    );
+            }
+            return produto;
+        }
+        public static List<Produto> ObterLista()
+        {
+            List<Produto> produtos = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandText = "select * from produtos order by descricao";
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                produtos.Add(new(
+                    dr.GetInt32(0),
+                    dr.GetString(1),
+                    dr.GetString(2),
+                    dr.GetDouble(3),
+                    dr.GetString(4),
+                    Categoria.ObterPorId(dr.GetInt32(5)),
+                    dr.GetDouble(6),
+                    dr.GetDouble(7),
+                    null,
+                    dr.GetDateTime(9)
+                    ));
+            }
+            return produtos;
+        }
     }
 }
